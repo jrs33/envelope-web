311132ed4aefd4a7a6b071af20933fe3bc999c91
Josh <surettej@bu.edu>
2020-04-02 22:10:45 -0400

    returning data in table instead

--- src/envelopes/envelopes_main.ts
+++ src/envelopes/envelopes_main.ts
@@ -11,30 +11,68 @@ function parseEnvelopes(envelopeJson) {
 	var envelopesDiv = document.createElement('div');
 	envelopesParent.appendChild(envelopesDiv);
 
-	var envelopeNameList = document.createElement('ul');
-	var envelopeAllocatedList = document.createElement('ul');
-	var envelopeSpentList = document.createElement('ul');
-	var envelopeRemainingList = document.createElement('ul');
-
-	envelopesDiv.appendChild(envelopeNameList);
-	envelopesDiv.appendChild(envelopeAllocatedList);
-	envelopesDiv.appendChild(envelopeSpentList);
-	envelopesDiv.appendChild(envelopeRemainingList);
-	for(const envelope of envelopeObject) {
-
-		let remaining = envelope.allocatedMoney - envelope.spentMoney;
-
-		appendList(envelope.name, envelopeNameList);
-		appendList(envelope.allocatedMoney, envelopeAllocatedList);
-		appendList(envelope.spentMoney, envelopeSpentList);
-		appendList(remaining, envelopeRemainingList);
-	}
+	const tableHeader = createTableHeader();
+	const tableBody = createTableBody(envelopeObject);
+	var table = document.createElement('table');
+	table.className = "table table-dark";
+	table.appendChild(tableHeader);
+	table.appendChild(tableBody);
+	envelopesDiv.appendChild(table);
 }
 
-function appendList(value, list) {
-	let newListEntry = document.createElement('li');
-	newListEntry.innerHTML = value;
-	list.appendChild(newListEntry);
+function createTableHeader() {
+
+	var tableHead = document.createElement('thead');
+	var tableHeadRow = document.createElement('tr');
+	tableHead.appendChild(tableHeadRow);
+
+	var nameCol = document.createElement('th');
+	var allocatedCol = document.createElement('th');
+	var spentCol = document.createElement('th');
+	var remainingCol = document.createElement('th');
+
+	nameCol.scope = "col";
+	nameCol.textContent = "Name";
+	allocatedCol.scope = "col";
+	allocatedCol.textContent = "Allocated";
+	spentCol.scope = "col";
+	spentCol.textContent = "Spent";
+	remainingCol.scope = "col";
+	remainingCol.textContent = "Remaining";
+	
+	tableHeadRow.appendChild(nameCol);
+	tableHeadRow.appendChild(allocatedCol);
+	tableHeadRow.appendChild(spentCol);
+	tableHeadRow.appendChild(remainingCol);
+
+	return tableHead;
+}
+
+function createTableBody(envelopeObject) {
+
+	var tableBody = document.createElement('tbody');
+	for(const envelope of envelopeObject) {
+
+		var tableRow = document.createElement('tr');
+
+		let remaining = envelope.allocatedMoney - envelope.spentMoney;
+		var nameData = document.createElement('td');
+		nameData.textContent = envelope.name;
+		var allocatedData = document.createElement('td');
+		allocatedData.textContent = envelope.allocatedMoney;
+		var spentData = document.createElement('td');
+		spentData.textContent = envelope.spentMoney;
+		var remainingData = document.createElement('td');
+		remainingData.textContent = remaining.toString();
+
+		tableRow.appendChild(nameData);
+		tableRow.appendChild(allocatedData);
+		tableRow.appendChild(spentData);
+		tableRow.appendChild(remainingData);
+		tableBody.appendChild(tableRow);
+	}
+
+	return tableBody;
 }
 
 export { getEnvelopes };
