c5aedf94bc1a4e44a39d207f390d6f71be5c23e0
Josh <surettej@bu.edu>
2020-05-23 18:24:34 -0400

    put everything in config for auth

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -5,8 +5,8 @@ const CONFIG = require('../../config.local.json');
 export const AUTH = new WebAuth({
     clientID: CONFIG.auth.client_id,
     domain: CONFIG.auth.domain,
-    responseType: 'token id_token',
-    audience: 'https://envelope-api.moneyme.com',
-    redirectUri: 'http://localhost:8081',
-    scope: 'openid profile read:envelopes create:envelopes read:transactions create:transactions'
+    responseType: CONFIG.auth.response_type,
+    audience: CONFIG.auth.audience,
+    redirectUri: CONFIG.auth.redirect_uri,
+    scope: CONFIG.auth.scope
   });
