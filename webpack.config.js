b4d7f8e85ba0ab0d5b8a8982539fa0df3eb96b7c
Josh <surettej@bu.edu>
2021-01-09 18:36:32 -0500

    use variable from webpack cli statement

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 
-// loads all values from .env into process.env
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({path: path.resolve(process.cwd(), '.env.development')});
-} else {
-  require('dotenv').config({path: path.resolve(process.cwd(), '.env')});
-}
-
-module.exports = {
+var config = {
   entry: './src/main.ts',
   devServer: {
     contentBase: './dist'
@@ -33,7 +26,7 @@ module.exports = {
       AUTH_REDIRECT_URI: '',
       AUTH_RESPONSE_TYPE: '',
       AUTH_SCOPE: '',
-      ENVELOPE_API_URL: 'http://localhost:8033',
+      ENVELOPE_API_URL: 'http://localhost:8083',
       NODE_ENV: 'development' // use 'development' unless process.env.NODE_ENV is defined
     })
   ],
@@ -41,4 +34,19 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+};
+
+module.exports = (env, argv) => {
+
+  // loads all values from .env into process.env
+  console.log('Build mode: ', argv.mode);
+  if (argv.mode === 'development') {
+    console.log('loading_dev_env_config')
+    require('dotenv').config({path: path.resolve(process.cwd(), '.env.development')});
+  } else {
+    console.log('loading_prod_env_config')
+    require('dotenv').config({path: path.resolve(process.cwd(), '.env')});
+  }
+
+  return config;
 };
